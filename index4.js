6d63d1c09a8e1e11ecb17667c011794d3f71fd05
manoel2020 <junior.miranda2100@gmail.com>
2024-03-14 13:46:51 -0400

    alteração teste

--- index4.js
+++ index4.js
@@ -1,9 +1,4 @@
-let nome = 'Git e GitHub';
+let nomeCurso = 'Git e GitHub';
 let cargaHoraria = '3 hrs';
 
-
-console.log(`nome do curso ${nome} carga horaria alteracao da main principal${cargaHoraria}`);
-
-console.log(`nome do curso ${nome} carga horaria alterado aqui${cargaHoraria} alteração para gerar conflito no merge`);
-console.log('teste 2');
-
+console.log(`Curso: ${nomeCurso} Carga Horaria: ${cargaHoraria}`);
